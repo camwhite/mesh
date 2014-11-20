c4cab40c93152ada68d8714dc6de2337169d43ce
Cameron White <camw85@mac.com>
2014-11-20 12:50:09 -0700

    minor changes and begin unit testing

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -7,7 +7,7 @@ angular.module('meshApp')
 
     var getObj = function() {
       $http.get('api/users/' + Auth.getCurrentUser()._id + '/things').success(function(data) {
-        $scope.objectives = data;
+        $scope.objectives = data.reverse();
       });
     }
     getObj();
@@ -50,13 +50,13 @@ angular.module('meshApp')
 
       if(form.$valid) {
         $scope.invalid = false;
-        $http.post('api/things', {name: $scope.objectiveToAdd, info: $scope.objDescription, contributors: $scope.invitees.length}).
-        success(function(data) {
-          $scope.objectives.push(data);
+        $http.post('api/things', {name: $scope.objectiveToAdd, info: $scope.objDescription, contributors: $scope.invitees.length + 1})
+        .success(function(data) {
+          $scope.objectives.unshift(data);
         });
 
         $scope.invitees = [];
-        
+
         $scope.objectiveToAdd = '';
         $scope.objDescription = '';
       }
@@ -65,6 +65,36 @@ angular.module('meshApp')
         $scope.addErrors.objective = 'An objective is required';
         $scope.addErrors.descript = 'A description is required'
       }
+    }
+
+
+
+    $scope.toggleEdit = function(index) {
+      //obj.active = !obj.active
+      $scope.indexEditing === index ? $scope.indexEditing = -1 : $scope.indexEditing = index;
+    }
+
+    $scope.editObjective = function(objective) {
+      var id = objective._id;
+      $http.put('api/things/' + id, objective).success(function() {
+        $http.get('api/users/' + Auth.getCurrentUser()._id + '/things').success(function(data) {
+          $scope.objectives = data.reverse();
+          $scope.indexEditing = -1;
+        });
+      }).error(function(status) {
+        console.log(status);
+      });
+    }
+
+    $scope.deleteObjective = function(objective) {
+      var id = objective._id;
+      $http.delete('api/things/' + id).success(function() {
+        $http.get('api/users/' + Auth.getCurrentUser()._id + '/things').success(function(data) {
+          $scope.objectives = data.reverse();
+        });
+      }).error(function(status) {
+        console.log(status);
+      });
     }
 
   });
--- client/app/dashboard/dashboard.controller.spec.js
+++ client/app/dashboard/dashboard.controller.spec.js
@@ -15,7 +15,64 @@ describe('Controller: DashboardCtrl', function () {
     });
   }));
 
-  it('should ...', function () {
-    expect(1).toEqual(1);
+  it('should remove an invitee from the list', function () {
+    scope.invitees = [
+      {
+        user: 'Irwin Litvak',
+        online: true
+      },
+      {
+        user: 'Jennifer Nelson',
+        online: true
+      },
+      {
+        user: 'Tyler Ball',
+        online: false
+      }
+    ];
+
+    scope.removeInvitee(scope.invitees[0])
+
+    expect(scope.invitees.length).toEqual(2);
   });
+
+  it('should toggle objective to add', function() {
+    var current = scope.objAdd;
+    
+    scope.toggleAdd();
+    
+    expect(current).toNotEqual(scope.objAdd);
+  });
+
+  it('should add an objective if valid', function() {
+
+  });
+
+  it('should delete an objective', function() {
+    scope.objectives = [
+      {
+        _id: "54667e9365545374216e4b3d",
+        name: "sadafd",
+        info: "sadadssa",
+        contributors: 2,
+        user: "545bc8f48a0f269a41c4822e",
+        __v: 0
+      },
+      {
+       _id: "54667e9365545374216e4b3d",
+        name: "sadafd",
+        info: "sadadssa",
+        contributors: 2,
+        user: "545bc8f48a0f269a41c4822e",
+        __v: 0 
+      }
+    ];
+
+    var objective = scope.objectives[0];
+
+    scope.deleteObjective(objective);
+
+    expect(scope.objectives.length).toEqual(1);
+  });
+
 });
