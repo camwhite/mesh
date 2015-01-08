adce7afc03eb494ccaf72bce6e27a7db18bc3e1d
Cameron White <camw85@mac.com>
2015-01-08 11:06:08 -0700

    started working on featured users

--- client/app/discover/discover.controller.js
+++ client/app/discover/discover.controller.js
@@ -6,13 +6,36 @@ angular.module('meshApp')
 
     $rootScope.toggleNav = true;
 
+    $scope.featuredUsers = [];
+
+    var myContacts = $scope.getCurrentUser().contacts.length;
+
     Http.getAllUsers().then(function(data) {
       $scope.standardUsers = data;
+      fillFeatured();
     });
+
+    var fillFeatured = function() {
+      while($scope.featuredUsers.length <= Math.min($scope.standardUsers.length - myContacts, 14)) {
+        var userIndex = Math.floor(Math.random() * ($scope.standardUsers.length - 1));
+        var found = false;
+        if($scope.knownContacts($scope.standardUsers[userIndex])) {
+          for(var i = 0; i < $scope.featuredUsers.length; i++) {
+            if($scope.featuredUsers[i]._id === $scope.standardUsers[userIndex]._id) {
+              found = true;
+            }
+          }
+        }
+        if(!found) {
+          $scope.featuredUsers.push($scope.standardUsers[userIndex]);
+          console.log($scope.featuredUsers)
+        }
+      }
+    }
 
     $scope.addAsContact = function(user) {
       var me = $scope.getCurrentUser();
-      Http.addAsContact(user).then(function (data) {
+      Http.addAsContact(user).then(function(data) {
         me.contacts.push(user);
         $rootScope.myContacts = me.contacts;
       });
@@ -22,6 +45,17 @@ angular.module('meshApp')
         .position('bottom right')
         .hideDelay(1500)
       );
+      removeFeatured(user);
+      fillFeatured();
+    }
+
+    var removeFeatured = function(user) {
+      for(var i = 0; i < $scope.featuredUsers.length; i++) {
+        if($scope.featuredUsers[i]._id === user._id) {
+          $scope.featuredUsers.splice(i, 1);
+          console.log($scope.featuredUsers);
+        }
+      }
     }
 
     $scope.knownContacts = function(user) {
